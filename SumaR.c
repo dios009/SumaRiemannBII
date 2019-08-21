7e34ff4c43e35168e157b25ee1d3f1a89eb45df4
dios009 <dyc1979@hotmail.com>
2019-08-21 13:42:56 -0300

    Add files via upload

---
+++ SumaR.c
@@ -0,0 +1,78 @@
+// Suma de Riemann de sigma
+
+#include <stdio.h>
+#include <math.h>
+//#include <quadmath.h>
+#include "pral.h"
+
+double bucle1(double p1, double theta1)
+{
+	double number = 0.;
+
+if(det(1.,100.,p1,theta1)>0.)
+{
+	double deltap2=(p2b(1.,100.,p1,theta1)-p2a(1.,100.,p1,theta1))/10000.;
+    double p2 = p2a(1.,100.,p1,theta1)+deltap2/1000.; /* En p2a hay problema, hace falta correr un poquito el inicio de la suma*/
+
+
+        do
+        {
+            if(deltap2>0.)
+	    {
+	        number += sigma(1.,1.,100.,p1,theta1,p2)*deltap2;
+		//printf("p2=%f \t" ,p2,theta1,p2);
+		//number += deltap2;
+		//printf("suma=%f \n" ,number);
+            }
+	    p2 += deltap2;
+        }
+        while(p2 <= p2b(1.,100.,p1,theta1));
+}
+	return number;
+}
+
+double bucle2(double p1)
+{
+	double number1 = 0., theta1=0., bb = 0., deltatheta1=theta1max(1.,100.)/10000.;
+	do
+	{
+	    bb=bucle1(p1,theta1);
+	    number1 += bb*deltatheta1;
+	    theta1 += deltatheta1;
+	 }
+	 while(theta1 <= theta1max(1.,100.));
+	return number1;
+}
+
+double bucle3(void)
+{
+	double number3 = 0., p1 = p1min(1.,100.) , cc = 0., deltap1= (p1max(1.,100.)-p1min(1.,100.))/100.;
+	
+	FILE *fptr;
+	fptr = fopen("datos-sr.txt","w");
+	do
+	{
+		cc = bucle2(p1);
+
+		number3 += cc*deltap1;
+		printf("\n p1=%f \t cc=%f \n",p1,cc);
+		fprintf(fptr,"%f \t %f \n",p1,cc);
+		p1 += deltap1;
+	}
+	while(p1 <= p1max(1.,100.)+p1max(1.,100.)/100000.);
+	fclose(fptr);
+	return number3;   
+
+}
+
+int main()
+{
+
+//printf("bucle1 = %f \n", bucle1(1,0.1));
+
+//printf("\n \n Suma de Riemann = %lf \n", bucle2(4));
+
+    printf("\n Suma de Riemann = %lf \n", bucle3());
+ 
+    return 0;
+}
